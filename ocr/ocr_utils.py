dc91daba4496fec48a5c7ec4a74b42dc2e3cdf3c
Abhiroop <abhiroop1998.dev@gmail.com>
2026-01-04 21:01:38 +0530

    Add Google Vision API integration and update requirements

--- ocr/ocr_utils.py
+++ ocr/ocr_utils.py
@@ -1,11 +1,14 @@
-import os
-
-from dotenv import load_dotenv
-from paddleocr import PaddleOCR
-from PIL import Image
-import numpy as np
 import io
+import os
+from typing import Dict
+
 import cv2
+import numpy as np
+from PIL import Image
+from dotenv import load_dotenv
+from fastapi import UploadFile
+from google.cloud import vision
+from paddleocr import PaddleOCR
 
 load_dotenv()
 # Initialize PaddleOCR ONCE (important)
@@ -16,7 +19,11 @@ ocr = PaddleOCR(
 
 os.environ["DISABLE_MODEL_SOURCE_CHECK"] = os.getenv("DISABLE_MODEL_SOURCE_CHECK")
 
-async def extract_text_from_image(file) -> dict:
+async def extract_text_from_image(file: UploadFile):
+    image_bytes = await file.read()
+    return extract_text_from_image_with_paddle_ocr(image_bytes)
+
+async def extract_text_from_image_with_paddle_ocr(file) -> dict:
     image_bytes = await file.read()
 
     image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
@@ -77,3 +84,33 @@ def split_question_answer(text: str):
         " ".join(question_lines),
         " ".join(answer_lines)
     )
+
+
+
+client = vision.ImageAnnotatorClient.from_service_account_file(os.getenv("GOOGLE_APPLICATION_CREDENTIALS"))
+
+def extract_text_google_vision_depreceated(image_bytes: bytes) -> Dict:
+    image = vision.Image(content=image_bytes)
+
+    response = client.text_detection(image=image)
+    annotations = response.text_annotations
+
+    if response.error.message:
+        raise RuntimeError(response.error.message)
+
+    if not annotations:
+        return {
+            "text": "",
+            "confidence": 0.0
+        }
+
+    # First annotation = full text
+    full_text = annotations[0].description
+
+    # Google Vision does NOT give a single confidence score reliably
+    avg_confidence = None
+
+    return {
+        "text": full_text,
+        "confidence": avg_confidence
+    }
